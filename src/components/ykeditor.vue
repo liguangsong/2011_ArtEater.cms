83655b72cf2bae3e5df366dda9b7e1cedb8b5297
yumianlinghu <zp88889@163.com>
2021-11-30 15:56:29 +0800

    调整

--- src/components/ykeditor.vue
+++ src/components/ykeditor.vue
@@ -121,10 +121,10 @@ export default {
         toolbar: {
           items: [
             //  'paragraph', 'heading1', 'heading2', 'heading3', 'heading4', 'heading5', 'heading6',
-            // 'heading',
-            // '|',
+            'heading',
+            '|',
             'bold',
-            // 'italic',
+            'italic',
             // 'link',
             '|',
             'fontFamily',
